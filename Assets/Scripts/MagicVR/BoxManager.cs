9d0702a17b7dc1562a3e48ea2e050b8e1132f949
jordantkay21 <112577991+jordantkay21@users.noreply.github.com>
2024-07-02 21:14:15 -0400

    https://medium.com/@jordantkay/unity-ar-magicbox-combo-logic-8e832c357085

---
+++ Assets/Scripts/MagicVR/BoxManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoxManager : MonoBehaviour
+{
+    private string correctGemOrder = "BlueRedGreen";
+    private string enteredGemOrder = "";
+
+    private int amountOfGems = 3;
+    private int currentGem = 0;
+
+    public Animator boxAnimator;
+    public UnityEvent gameIsWon;
+    
+    public void GemSelect(Gem currentSelectGem)
+    {
+        enteredGemOrder += currentSelectGem.gemColorName;
+        currentGem += 1;
+
+        if (currentGem == 3)
+            CompareGemOrder();
+
+        print(currentSelectGem.gemColorName + " Gem Selected!");
+    }
+
+    public void CompareGemOrder()
+    {
+        if (enteredGemOrder == correctGemOrder)
+        {
+            print("Order is Correct!");
+            CompleteGame();
+        }
+        else
+        {
+            print("Order in Incorrect, resetting");
+            print("User entered: " + enteredGemOrder);
+            ResetGame();
+        }
+    }
+    private void CompleteGame()
+    {
+        gameIsWon.Invoke();
+    }
+
+    private void ResetGame()
+    {
+        currentGem = 0;
+        enteredGemOrder = "";
+    }
+
+}
